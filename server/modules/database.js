efe44402eb5aaee438e70e78d1214b450c551ad8
Enzuo <potc.zone+git@gmail.com>
2019-10-05 21:52:29 +0200

    Fix massivejs with new version

--- server/modules/database.js
+++ server/modules/database.js
@@ -6,13 +6,9 @@ var path = require('path')
 var connectionString = 'postgres://'+config.get('database.username')+':'+config.get('database.password')+'@'+config.get('database.host')+'/'+config.get('database.name')
 
 module.exports = {
-	init : function () {
-		return new Promise((resolve, reject) => {
-			debug('establishing connection...')
-			massive.connect({connectionString, scripts : path.join('server', 'db')}, function(err, db){
-				if (err) return reject(err)
-				resolve(db)
-			})
-		})
+	init : async function () {
+		debug('establishing connection...')
+		const db = await massive({connectionString, scripts : path.join('server', 'bdb')});
+		return db
 	}
 }
